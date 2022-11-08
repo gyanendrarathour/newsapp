edda81a795be5887699445579a0ca1d66ba0702f
gyanendrarathour <krishurathour1992@gmail.com>
2022-11-09 01:06:50 +0530

    Home Page Completed

---
+++ lib/view/home.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:newsapp/view/widgets/newsContainer.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: PageView.builder(
+            scrollDirection: Axis.vertical,
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return NewsContainer(
+                  imgUrl:
+                      'https://images.unsplash.com/photo-1588681664899-f142ff2dc9b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80',
+                  newsHead: '5G in India',
+                  newsDesc:
+                      'Reliance Jio’s 5G service will be available in four cities of Delhi, Mumbai, Kolkata and Varanasi initially before it is expanded across the country by December 2023. The service provider decided to launch on the day of Dussehra (a major Indian festival celebrating the victory of good over evil) on October 5. The Jio subscribers will get unlimited data with 1 Gbps speed as part of the True 5G Welcome offer.',
+                  newsUrl:
+                      'https://www.fiercewireless.com/5g/india-says-hello-5g');
+            }),
+      ),
+    );
+  }
+}
---
+++ lib/view/widgets/newsContainer.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class NewsContainer extends StatelessWidget {
+  String imgUrl;
+  String newsHead;
+  String newsDesc;
+  String newsUrl;
+  NewsContainer(
+      {super.key,
+      required this.imgUrl,
+      required this.newsHead,
+      required this.newsDesc,
+      required this.newsUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Image.network(
+            imgUrl,
+            height: 400,
+            width: MediaQuery.of(context).size.width,
+            fit: BoxFit.cover,
+          ),
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Text(
+                  newsHead,
+                  style: const TextStyle(
+                      fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  newsDesc,
+                  style: const TextStyle(fontSize: 15),
+                )
+              ],
+            ),
+          ),
+          const Spacer(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: ElevatedButton(
+                    onPressed: () {
+                      print('Going to $newsUrl');
+                    },
+                    child: Text('Read More')),
+              ),
+            ],
+          ),
+          const SizedBox(
+            height: 20,
+          )
+        ],
+      ),
+    );
+  }
+}
